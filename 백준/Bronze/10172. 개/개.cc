391340597c5356b31016b8441cb1f4d19441c251
은정 방 <108053947+ebang091@users.noreply.github.com>
2023-01-06 19:12:02 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+/*
+ *  A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+ */
+
+using namespace std;
+vector <int> num(4);
+vector <int> number(10);
+
+
+int main()
+{
+
+   cout << "|\\_/|" << "\n";
+   cout <<"|q p|   /}" << "\n";
+   cout << "( 0 )\"\"\"\\" << "\n";
+   cout << "|\"^\"`    |" << "\n";
+   cout << "||_/=\\\\__|" << "\n";
+
+
+}
+
