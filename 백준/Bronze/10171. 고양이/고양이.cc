645073bf32c4184cbd664d2ac6a5b0e4ba0567fa
은정 방 <108053947+ebang091@users.noreply.github.com>
2023-01-06 19:10:10 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+/*
+ *  A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+ */
+
+using namespace std;
+vector <int> num(4);
+vector <int> number(10);
+
+
+int main()
+{
+
+   cout << "\\    /\\" << "\n";
+   cout <<" )  ( ')" << "\n";
+   cout << "(  /  )" << "\n";
+   cout << " \\(__)|" << "\n";
+
+
+}
+
