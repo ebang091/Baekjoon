606fb0b9829657f5258c80e56dda06e9b1845216
은정 방 <108053947+ebang091@users.noreply.github.com>
2023-03-15 20:41:31 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+             
+int main()
+{
+    printf("         ,r'\"7");
+    printf("\n");
+    printf("r`-_   ,'  ,/");
+    printf("\n");
+    printf(" \\. \". L_r'");
+    printf("\n");
+    printf("   `~\\/");
+   printf("\n");
+    printf("      |");
+    printf("\n");
+    printf("      |");
+                          
+                 
+}
