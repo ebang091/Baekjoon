93a0e0acf29e79a0c6a2db3ee8ea2457d583f64c
은정 방 <108053947+ebang091@users.noreply.github.com>
2023-03-20 18:55:17 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -10,7 +10,7 @@ int main()
 	{
 		getline(cin, input);  // 앤터가 나오기 전 까지 한 줄 입력 
 
-		if (input == "")  // 공백이 입력되면 종료 
+		if (cin.eof())  // 공백이 입력되면 종료 
 			break;
 
 		cout << input  << endl;  // 입력된 한 줄 출력 
