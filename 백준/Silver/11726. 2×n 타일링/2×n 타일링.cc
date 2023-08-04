553894a9cb6ba3b22fa81ce88315ab931c84a829
은정 방 <108053947+ebang091@users.noreply.github.com>
2023-08-04 11:59:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <cstring>
+#include <sstream>
+#define FAST cin.tie(NULL); ios::sync_with_stdio(false);
+using namespace std;
+
+#define NONE -1
+#define INF 198765432
+long long dp[1001];
+int N;
+int main(){
+    FAST
+    cin >> N;
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3; i <= N; i++)
+        dp[i] = (dp[i-1] % 10007 + dp[i-2] % 10007 ) % 10007 ;
+    cout << dp[N];
+
+    
+
+}
