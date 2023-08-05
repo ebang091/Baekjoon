8fb7b72b3e91f4ae1a8f2f21b41ad21c056f0978
은정 방 <108053947+ebang091@users.noreply.github.com>
2023-08-05 13:36:13 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,45 +1,60 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <cstring>
+#include <sstream>
+#define FAST cin.tie(NULL); ios::sync_with_stdio(false);
+using namespace std;
 
-int num[100001];
-int N;
-int sum[100001];
-int min[100001];
+#define NONE -1
+#define INF 19876543
+// #define SIZE (1 << 17)
+long long SIZE;
+vector<int> tree;
+int nums[100001];
+int dp1[100001];
+int N,T;
+/*
+특정 위치까지의 가장 긴 증가하는 수열 +  특정 위치부터 가장 긴 감소하는 수열
+dp1[i] = i 위치까지 가장 긴 증가하는 수열의 길이
+dp[i] = max(dp[j] + 1, dp2[i]) j < i, aj < ai
 
-int main()
-{
-    int max=-1987654;
-    int ret,minnum;
-    scanf("%d", &N);
-    for(int i=0;i<N;i++)
-    {
-        scanf("%d", &num[i]);
+dp2[i] = max(dp[j] + 1, dp2[i]) j > i, aj > ai
+
+dp1[i] + dp[2] 의 합 중 가장 큰 값을 선정한다.
+*/
+
+long long max(int a, int b){
+    return a > b ? a: b;
+}
+
+int main(){
+    FAST
+    cin >> N;
+    int n;
+    int maxV = -INF;
+    for(int i = 0; i < N ; i++){
+        cin >> nums[i];
     }
-    sum[0] =num[0];
-    for(int i=1;i<N;i++)
-        sum[i]= sum[i-1] + num[i];
-    minnum = sum[0];
-    for(int i=0;i<N;i++)
-    {
-        min[i] = minnum;
-        if(sum[i] < minnum)
-            minnum = sum[i];
-    }
-//
-//    printf("\n");
-//    for(int i=0;i<N;i++)
-//        printf(" %d", min[i]);
-    for(int i=0;i<N;i++)
-    {
-        if(i != 0)
-            ret = sum[i]-min[i];
-        else
-            ret = sum[i];
-       if(ret < sum[i])
-           ret = sum[i];
-       //printf("%d ", ret);
-       if(ret > max)
-           max = ret;
-    }//printf("\n");
-    printf("%d", max);
 
+    for(int i = 0 ; i < N; i++){
+        dp1[i] = nums[i];
+        dp1[i] = max(dp1[i], dp1[i -1] + nums[i]);
+        if(dp1[i] > maxV)
+            maxV = dp1[i];
+    }
+
+    cout << maxV;
+
+
+
+    
+
+
+    
 }
