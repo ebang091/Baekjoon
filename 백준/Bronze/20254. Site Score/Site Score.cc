3a1c20fc4151cd92d1b440231503be587372c871
은정 방 <108053947+ebang091@users.noreply.github.com>
2023-08-27 23:57:17 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,10 @@
+/*
+
+56UR + 24TR + 14UO + 6TO*/
+#include <iostream>
+using namespace std;
+int main(){
+     int a, b, c,d;
+    cin >> a >> b >> c >> d;
+    cout << (a * 56 + 24 * b + 14 * c + 6 * d);
+}
