1d4e2b842bda02815fd10b3f7993ccf7bd780212
은정 방 <108053947+ebang091@users.noreply.github.com>
2023-09-25 23:01:21 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+long long N, M;
+int main(){
+    cin >> N >> M;
+    if(N * 100 >= M)
+            cout << "Yes";
+       else
+           cout << "No";
+    
+}
