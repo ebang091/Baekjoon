cc4fe213b1702890b640d8e2fb146d99e90b4dfb
eebb <108053947+ebang091@users.noreply.github.com>
2023-12-20 21:32:50 +0900

    [Bronze IV] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long long  N,a,b;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+         cin >> a >> b;
+        if(a >= b)
+           cout << "MMM BRAINS\n";
+        else
+        cout << "NO BRAINS\n";
+        }
+    }
+   
