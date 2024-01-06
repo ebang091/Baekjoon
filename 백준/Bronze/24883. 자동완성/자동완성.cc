b3e637a7598064d1ded0734d2f4dae5ac16b9521
eebb <108053947+ebang091@users.noreply.github.com>
2024-01-07 00:36:34 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string input;
+    cin >> input;
+    if(input == "N" || input == "n")
+            cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+}
+    
